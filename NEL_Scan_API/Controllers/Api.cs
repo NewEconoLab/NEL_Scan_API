68d3951a254c7bc690dbd316121a7bc6325faa3f
tssqzr <1214661466@qq.com>
2018-07-24 14:53:15 +0800

    add NNS Event function list

---
+++ NEL_Scan_API/Controllers/Api.cs
@@ -0,0 +1,155 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
+using NEL_Scan_API.Service;
+using NEL_Agency_API.RPC;
+using NEL_Scan_API.lib;
+
+namespace NEL_Scan_API.Controllers
+{
+    public class Api
+    {
+        private string netnode { get; set; }
+        private string mongodbConnStr { get; set; }
+        private string mongodbDatabase { get; set; }
+        private string notify_mongodbConnStr { get; set; }
+        private string notify_mongodbDatabase { get; set; }
+        private string nelJsonRPCUrl { get; set; }
+        private string mongodbConnStrAtBlock { get; set; }
+        private string mongodbDatabaseAtBlock { get; set; }
+    
+        //private OssFileService ossClient;
+        //private AuctionService auctionService;
+        //private BonusService bonusService;
+        private AssetService assetService;
+        private NNSService nnsService;
+        private mongoHelper mh = new mongoHelper();
+
+        public Api(string node)
+        {
+            netnode = node;
+            switch (netnode)
+            {
+                case "testnet":
+                    mongodbConnStr = mh.mongodbConnStr_testnet;
+                    mongodbDatabase = mh.mongodbDatabase_testnet;
+                    notify_mongodbConnStr = mh.notify_mongodbConnStr_testnet;
+                    notify_mongodbDatabase = mh.notify_mongodbDatabase_testnet;
+                    nelJsonRPCUrl = mh.nelJsonRPCUrl_testnet;
+                    mongodbConnStrAtBlock = mh.mongodbConnStrAtBlock_testnet;
+                    mongodbDatabaseAtBlock = mh.mongodbDatabaseAtBlock_testnet;
+                    assetService = new AssetService
+                    {
+                        mongodbConnStr = mh.mongodbConnStrAtBlock_testnet,
+                        mongodbDatabase = mh.mongodbDatabaseAtBlock_testnet,
+                        mh = mh
+                    };
+                    nnsService = new NNSService
+                    {
+                        newNotify_mongodbConnStr = mh.notify_mongodbConnStr_testnet,
+                        newNotify_mongodbDatabase = mh.notify_mongodbDatabase_testnet,
+                        nnsDomainState = mh.nnsDomainState_testnet,
+                        mh = mh,
+                    };
+                    break;
+                case "mainnet":
+                    mongodbConnStr = mh.mongodbConnStr_mainnet;
+                    mongodbDatabase = mh.mongodbDatabase_mainnet;
+                    notify_mongodbConnStr = mh.notify_mongodbConnStr_mainnet;
+                    notify_mongodbDatabase = mh.notify_mongodbDatabase_mainnet;
+                    nelJsonRPCUrl = mh.nelJsonRPCUrl_mainnet;
+                    mongodbConnStrAtBlock = mh.mongodbConnStrAtBlock_mainnet;
+                    mongodbDatabaseAtBlock = mh.mongodbDatabaseAtBlock_mainnet;
+                    assetService = new AssetService
+                    {
+                        mongodbConnStr = mh.mongodbConnStrAtBlock_mainnet,
+                        mongodbDatabase = mh.mongodbDatabaseAtBlock_mainnet,
+                        mh = mh
+                    };
+                    nnsService = new NNSService
+                    {
+                        newNotify_mongodbConnStr = mh.notify_mongodbConnStr_mainnet,
+                        newNotify_mongodbDatabase = mh.notify_mongodbDatabase_mainnet,
+                        nnsDomainState = mh.nnsDomainState_mainnet,
+                        mh = mh,
+                    };
+                    break;
+            }
+        }
+
+        public object getRes(JsonRPCrequest req, string reqAddr)
+        {
+            JArray result = null;
+            try
+            {
+                switch (req.method)
+                {
+                    // 获取域名信息
+                    case "getdomaininfo":
+                        result = nnsService.getDomain(req.@params[0].ToString());
+                        break;
+                    // 最具价值域名
+                    case "getaucteddomain":
+                        if (req.@params.Length < 2)
+                        {
+                            result = nnsService.getUsedDomainList();
+                        }
+                        else
+                        {
+                            result = nnsService.getUsedDomainList(int.Parse(req.@params[0].ToString()), int.Parse(req.@params[1].ToString()));
+                        }
+                        break;
+                    // 正在竞拍域名
+                    case "getauctingdomain":
+                        if (req.@params.Length < 2)
+                        {
+                            result = nnsService.getAuctingDomainList();
+                        }
+                        else
+                        {
+                            result = nnsService.getAuctingDomainList(int.Parse(req.@params[0].ToString()), int.Parse(req.@params[1].ToString()));
+                        }
+                        break;
+                    // statistics(奖金池+已领分红+已使用域名数量+正在竞拍域名数量)
+                    case "getstatistics":
+                        result = nnsService.getStatistic();
+                        break;
+                    // 资产名称模糊查询
+                    case "fuzzysearchasset":
+                        result = assetService.fuzzySearchAsset(req.@params[0].ToString());
+                        break;
+                    // 根据地址查询txid列表
+                    case "gettxidsetbyaddress":
+                        string queryTxidSetAddr = "{$or: [{\"from\":\"" + req.@params[0] + "\"}" + "," + "{\"to\":\"" + req.@params[0] + "\"}]}";
+                        JArray queryTxidSetRes = mh.GetData(notify_mongodbConnStr, notify_mongodbDatabase, "0x4ac464f84f50d3f902c2f0ca1658bfaa454ddfbf", queryTxidSetAddr);
+                        if (queryTxidSetRes == null || queryTxidSetRes.Count() == 0)
+                            result = new JArray { };
+                        else
+                            result = new JArray() { queryTxidSetRes.Select(item => item["txid"].ToString()).ToArray() };
+                        break;
+                }
+                if (result.Count == 0)
+                {
+                    JsonPRCresponse_Error resE = new JsonPRCresponse_Error(req.id, -1, "No Data", "Data does not exist");
+
+                    return resE;
+                }
+            }
+            catch (Exception e)
+            {
+                JsonPRCresponse_Error resE = new JsonPRCresponse_Error(req.id, -100, "Parameter Error", e.Message);
+
+                return resE;
+
+            }
+
+            JsonPRCresponse res = new JsonPRCresponse();
+            res.jsonrpc = req.jsonrpc;
+            res.id = req.id;
+            res.result = result;
+
+            return res;
+        }
+    }
+}
+
---
+++ NEL_Scan_API/Service/AssetService.cs
@@ -0,0 +1,131 @@
+﻿using NEL_Scan_API.lib;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NEL_Scan_API.Service
+{
+    public class AssetService
+    {
+        public string mongodbConnStr { set; get; }
+        public string mongodbDatabase { set; get; }
+        public mongoHelper mh { set; get; }
+
+
+        public JArray fuzzySearchAsset(string name, int pageNum = 1, int pageSize = 6)
+        {
+            JArray res1 = search(false, "asset", name, pageNum, pageSize);
+            JArray res2 = search(true, "NEP5asset", name, pageNum, pageSize);
+            List<JToken> list7 = new List<JToken>();
+            foreach (var item in res1)
+            {
+                list7.Add(item);
+            }
+            foreach (var item in res2)
+            {
+                list7.Add(item);
+            }
+            return new JArray()
+            {
+                list7.Take(pageSize).ToArray()
+            };
+        }
+
+        private JObject newOrFilter(string key, string regex)
+        {
+            JObject obj = new JObject();
+            JObject subobj = new JObject();
+            subobj.Add("$regex", regex);
+            subobj.Add("$options", "i");
+            obj.Add(key, subobj);
+            return obj;
+        }
+        private JArray search(bool isNep5, string coll, string name, int pageNum = 1, int pageSize = 6)
+        {
+            string key = isNep5 ? "name" : "name.name";
+            JObject orFilter = new JObject();
+            JArray orFilterSub = new JArray();
+            orFilterSub.Add(newOrFilter(key, name));
+            orFilterSub.Add(newOrFilter(key, transferName(name)));
+            orFilter.Add("$or", orFilterSub);
+
+            JArray res = mh.GetDataPages(mongodbConnStr, mongodbDatabase, coll, "{}", pageSize, pageNum, orFilter.ToString());
+            if (res == null || res.Count == 0)
+            {
+                return new JArray() { };
+            }
+
+            if (isNep5)
+            {
+                return new JArray() {{
+                res.Select(item => {
+                    string id = Convert.ToString(item["assetid"]);
+                    JObject obj = new JObject();
+                    obj.Add("assetid", id);
+                    obj.Add("name", transferRes(id, Convert.ToString(item["name"])));
+                    return obj;
+                }).GroupBy(pItem => pItem["assetid"], (k,g) => g.ToArray()[0]).Where(pp => !isNeoOrGas(pp["assetid"].ToString())).ToArray()
+            } };
+            }
+            return new JArray() {{
+                res.SelectMany(item => {
+                    string id = Convert.ToString(item["id"]);
+                    return item["name"].Select(subItem =>
+                    {
+                        JObject obj = new JObject();
+                        obj.Add("assetid", id);
+                        obj.Add("name", transferRes(id, Convert.ToString(subItem["name"])));
+                        return obj;
+                    }).ToArray();
+                }).GroupBy(pItem => pItem["assetid"], (k,g) => g.ToArray()[0]).ToArray()
+            } };
+        }
+
+        private string transferName(string name)
+        {
+            if (neoFilter.Contains(name.ToLower()))
+            {
+                return "AntShare";
+            }
+            if (gasFilter.Contains(name.ToLower()))
+            {
+                return "AntCoin";
+            }
+            return name;
+        }
+        private string[] neoFilter = new string[]
+        {
+            "n","ne","neo"
+        };
+
+        private string[] gasFilter = new string[]
+        {
+            "g","ga","gas"
+        };
+
+        private string transferRes(string id, string name)
+        {
+            if (id == id_neo)
+            {
+                return "NEO";
+            }
+            if (id == id_gas)
+            {
+                return "GAS";
+            }
+            return name;
+        }
+        private bool isNeoOrGas(string assetid)
+        {
+            if (assetid == id_gas || assetid == id_neo)
+            {
+                return true;
+            }
+            return false;
+        }
+        private string id_neo = "0xc56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b";
+        private string id_gas = "0x602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7";
+
+    }
+}
