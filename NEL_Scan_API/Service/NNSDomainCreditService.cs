bd9b0b928c56f31310d3ada6063cbcca3636c46e
tssqzr <1214661466@qq.com>
2019-04-03 17:27:05 +0800

    新增域名所有权变更的条件判断

--- NEL_Scan_API/Service/NNSDomainCreditService.cs
+++ NEL_Scan_API/Service/NNSDomainCreditService.cs
@@ -14,7 +14,20 @@ namespace NEL_Scan_API.Service
             string findStr = new JObject() { {"address", address } }.ToString();
             string fieldStr = new JObject() { {"fulldomain", 1 },{ "ttl", 1 },{ "_id",0} }.ToString();
             var queryRes = mh.GetDataWithField(mongodbConnStr, mongodbDatabase, "nnsDomainCreditState", fieldStr, findStr);
-            return queryRes;
+
+            var item = queryRes[0];
+            var namehash = item["namehash"].ToString();
+            findStr = new JObject() { { "namehash", namehash } }.ToString();
+            fieldStr = new JObject() { { "owner", 1 }, { "TTL", 1 }, { "_id", 0 } }.ToString();
+            queryRes = mh.GetDataWithField(mongodbConnStr, mongodbDatabase, "domainOwnerCol", fieldStr, findStr);
+            if (queryRes != null && queryRes.Count > 0)
+            {
+                if (queryRes[0]["owner"].ToString() != address || long.Parse(queryRes[0]["TTL"].ToString()) < TimeHelper.GetTimeStamp())
+                {
+                    return new JArray();
+                }
+            }
+            return new JArray { new JObject { { "fulldomain", item["fulldomain"] } } };
         }
     }
 }
