8ca0f964954c1ae3c4b8ab6d7e94f62802041a06
wowoyinwei <82604458@qq.com>
2019-04-10 16:20:25 +0800

    更新获取资产排名的接口

--- NEL_Scan_API/Service/AnalyService.cs
+++ NEL_Scan_API/Service/AnalyService.cs
@@ -83,15 +83,20 @@ namespace NEL_Scan_API.Service
         
         public JArray getRankByAsset(string asset, int pageSize, int pageNum)
         {
-            JObject filter = new JObject() { { "asset", asset } };
-            JObject sort = new JObject() { { "balance", -1 } };
-            JArray res = mh.GetDataPages(analy_mongodbConnStr, analy_mongodbDatabase, "allAssetRank", sort.ToString(), pageSize, pageNum, filter.ToString());
+            JObject filter = new JObject() { { "AssetHash", asset } };
+            JObject sort = new JObject() { { "Balance", -1 } };
+            JArray res = mh.GetDataPages(analy_mongodbConnStr, analy_mongodbDatabase, "address_assetid_balance", sort.ToString(), pageSize, pageNum, filter.ToString());
+            for (var i = 0;i<res.Count;i++)
+            {
+                JObject jo = (JObject)res[i];
+                res[i] = new JObject() { { "asset", (string)jo["AssetHash"] }, { "balance",jo["Balance"]["$numberDecimal"] } ,{ "addr",jo["Address"]} };
+            }
             return res;
         }
         public JArray getRankByAssetCount(string asset)
         {
-            JObject filter = new JObject() { { "asset", asset } };
-            long res = mh.GetDataCount(analy_mongodbConnStr, analy_mongodbDatabase, "allAssetRank", filter.ToString());
+            JObject filter = new JObject() { { "AssetHash", asset } };
+            long res = mh.GetDataCount(analy_mongodbConnStr, analy_mongodbDatabase, "address_assetid_balance", filter.ToString());
             return getJAbyKV("count", res);
         }
 
---
+++ NEL_Scan_API/lib/NumberDecimalHelper.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Globalization;
+
+namespace NEL_Scan_API.lib
+{
+    public class NumberDecimalHelper
+    {
+        public static string formatDecimal(string numberDecimalStr)
+        {
+            string value = numberDecimalStr;
+            if (numberDecimalStr.Contains("$numberDecimal"))
+            {
+                value = Convert.ToString(JObject.Parse(numberDecimalStr)["$numberDecimal"]);
+            }
+            if (numberDecimalStr.Contains("_csharpnull"))
+            {
+                value = "0";
+            }
+            if (value.Contains("E"))
+            {
+                value = decimal.Parse(value, NumberStyles.Float).ToString();
+            }
+            return value;
+        }
+    }
+}
